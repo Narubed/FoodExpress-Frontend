1b808d045f75620d3b144ee8099967f10faa80a6
Narubed Kaewserd <llaofllnolnw@gmail.com>
2022-01-04 10:32:26 +0700

    add config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-unresolved */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  // eslint-disable-next-line global-require
+  plugins: [require('@tailwindcss/forms')]
+};
